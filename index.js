8ff7a4f88841ee456a74bef7034ed97c1e63bb5f
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-06 08:46:21 -0600

    aun no funciona

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hello, World!');
+    res.send('¡Hello, World!');
 });
 
 app.listen(port, () => {
