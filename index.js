dadb3d821be516fe9bc78c7f5189986cd3f23f59
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-07 14:23:28 -0600

    nose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('! h World!');
+    res.send('! hello World!');
 });
 
 app.listen(port, () => {
