a1791c93b118fd0681db328148ce3a9c8696bdd5
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-08 23:11:11 -0600

    listo

--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const express = require('express');
 
 const app = express();
 app.get('/api/v1/welcome', (req, res) => {
-    res.status(200).send('Hello World');
+    res.status(200).send('hola daniel');
 });
 
 describe('GET /api/v1/welcome', () => {
-    it('respond with hola daniel!', (done) => {
+    it('respond with hola daniel', (done) => {
         request(app)
             .get('/api/v1/welcome')
-            .expect('hola daniel!', done);
+            .expect('hola daniel', done);
     });
 });
