4fa62029a8cb64c574ca106ac6bd3f2686f33677
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-08 23:08:21 -0600

    listoooo

--- test.js
+++ test.js
@@ -7,9 +7,9 @@ app.get('/api/v1/welcome', (req, res) => {
 });
 
 describe('GET /api/v1/welcome', () => {
-    it('respond with Hello, World!', (done) => {
+    it('respond with hola daniel!', (done) => {
         request(app)
             .get('/api/v1/welcome')
-            .expect('Hello, World!', done);
+            .expect('hola daniel!', done);
     });
 });
