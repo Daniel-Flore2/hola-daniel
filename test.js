c0ff4e06fe8dea53fc50a9e12a3a91b885e845fa
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-12 13:47:50 -0600

    change msg welcome

--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const express = require('express');
 
 const app = express();
 app.get('/api/v1/welcome', (req, res) => {
-    res.status(200).send('hola daniel');
+    res.status(200).send('hola danielito');
 });
 
 describe('GET /api/v1/welcome', () => {
-    it('respond with hola daniel', (done) => {
+    it('respond with hola danielito', (done) => {
         request(app)
             .get('/api/v1/welcome')
-            .expect('hola daniel', done);
+            .expect('hola danielito', done);
     });
 });
